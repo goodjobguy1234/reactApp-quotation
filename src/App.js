d6fbd55f8afc124353a4cb78a21a806021ec2ea2
goodjobguy1234 <goodjobguy1234@gmail.com>
2022-01-04 20:34:30 +0700

    finish class

--- src/App.js
+++ src/App.js
@@ -1,36 +1,98 @@
-import logo from "./logo.svg";
-import NameList from "./NameList";
-import "./App.css";
+import { useState, useRef, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import QuotationTable from "./QuotationTable";
+
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+import useLocalStorage from 'react-localstorage-hook'
 
 function App() {
-  const s1 = {
-    fontSize: "20px",
-    textAlign: "left",
-    paddingLeft: "10px",
-    paddingRight: "10px",
+  const itemRef = useRef();
+  const ppuRef = useRef();
+  const qtyRef = useRef();
+
+  // const [dataItems, setDataItems] = useState([]);
+  const [dataItems, setDataItems] = useLocalStorage("dataItems",[]);
+  
+
+
+  const dummyProductList = [
+    { id: "p001", name: 'Samsung LED TV 52"', price: 15000 },
+    { id: "p002", name: "Xiaomi Air Purifier", price: 4000 },
+    { id: "p003", name: "Brother Laserjet Printer", price: 5500 },
+    { id: "p004", name: "Bose Speaker", price: 20000 },
+  ];
+
+  const addItem = () => {
+    if (itemRef.current.value == "") {
+      alert("Item name is empty");
+      return;
+    }
+
+    const pid = itemRef.current.value;
+    const product = dummyProductList.find((e) => e.id === pid);
+
+    var itemObj = {
+      pid: pid,
+      item: product.name,
+      ppu: ppuRef.current.value,
+      qty: qtyRef.current.value,
+    };
+
+    dataItems.push(itemObj);
+    setDataItems([...dataItems]);   
   };
 
+  const productChange = (e) => {
+    const pid = itemRef.current.value;
+    const product = dummyProductList.find((e) => e.id === pid);
+    ppuRef.current.value = product.price
+  }
+
+  const options = dummyProductList.map((v) => {
+    return <option value={v.id}>{v.name}</option>;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Quotation</p>
-      </header>
-      <div className="App-body">
-        <h1>Name List</h1>
-        <NameList />
-      </div>
-      <div style={s1}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={5} style={{ backgroundColor: "#eaeaea" }}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formItem">
+              <Form.Label>Item</Form.Label>
+              <Form.Select
+                aria-label="Default select example"
+                ref={itemRef}
+                onChange={productChange}
+              >
+                {options}
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Price Per Unit"
+                ref={ppuRef}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formQauntity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control type="number" placeholder="Quantity" ref={qtyRef} />
+            </Form.Group>
+
+            <Button variant="outline-dark" onClick={addItem}>
+              Add
+            </Button>
+          </Form>
+        </Col>
+        <Col>
+          <QuotationTable data={dataItems} setDataItems={setDataItems} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
